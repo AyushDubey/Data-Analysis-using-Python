6c2ddffa7a0ce53224776c934b09c00268e0bf8c
Ayush Dubey <ayush.dubey88@gmail.com>
2016-07-25 14:19:02 +0530

    Update ConfigParser_intro.py

--- ConfigParser_intro.py
+++ ConfigParser_intro.py
@@ -6,9 +6,11 @@ Created on Mon Jul 25 13:05:47 2016
 """
 # importing ConfigParser
 import ConfigParser
+
 # creating instance of ConfigParser.RawConfigParser
 config = ConfigParser.RawConfigParser()
 print config
 
-print config.read("config.cfg")
+config.read("config.cfg")
+
 print config.get('classifier','port')
