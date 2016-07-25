5a379c99e02b8f2793e25c2a4d83ce2290bad140
Ayush Dubey <ayush.dubey88@gmail.com>
2016-07-25 14:18:08 +0530

    Add files via upload

--- ConfigParser_intro.py
+++ ConfigParser_intro.py
@@ -4,5 +4,11 @@ Created on Mon Jul 25 13:05:47 2016
 
 @author: AY355513
 """
-
+# importing ConfigParser
 import ConfigParser
+# creating instance of ConfigParser.RawConfigParser
+config = ConfigParser.RawConfigParser()
+print config
+
+print config.read("config.cfg")
+print config.get('classifier','port')
